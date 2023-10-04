82663868072e55b4cb2001d573ddebcceb5ea628
slinnscott <sdslinn@gmail.com>
2023-10-03 21:26:23 -0600

    Adding mock sql data

---
+++ MockData/authors.sql
@@ -0,0 +1,32 @@
+create table author (
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	gender VARCHAR(50)
+);
+insert into author (id, first_name, last_name, email, gender) values (1, 'Dietrich', 'Ferras', 'dferras0@mayoclinic.com', 'Male');
+insert into author (id, first_name, last_name, email, gender) values (2, 'Locke', 'Maleby', 'lmaleby1@globo.com', 'Male');
+insert into author (id, first_name, last_name, email, gender) values (3, 'Masha', 'Ridsdale', 'mridsdale2@seattletimes.com', 'Female');
+insert into author (id, first_name, last_name, email, gender) values (4, 'Maddy', 'McLay', 'mmclay3@godaddy.com', 'Non-binary');
+insert into author (id, first_name, last_name, email, gender) values (5, 'Aland', 'Goodwill', 'agoodwill4@domainmarket.com', 'Male');
+insert into author (id, first_name, last_name, email, gender) values (6, 'Livvyy', 'Syson', 'lsyson5@themeforest.net', 'Female');
+insert into author (id, first_name, last_name, email, gender) values (7, 'Annette', 'Boarder', 'aboarder6@europa.eu', 'Female');
+insert into author (id, first_name, last_name, email, gender) values (8, 'Rubina', 'Radleigh', 'rradleigh7@a8.net', 'Polygender');
+insert into author (id, first_name, last_name, email, gender) values (9, 'Uri', 'Molesworth', 'umolesworth8@blog.com', 'Male');
+insert into author (id, first_name, last_name, email, gender) values (10, 'Noland', 'O'' Clovan', 'noclovan9@tamu.edu', 'Male');
+insert into author (id, first_name, last_name, email, gender) values (11, 'Redford', 'Firidolfi', 'rfiridolfia@issuu.com', 'Male');
+insert into author (id, first_name, last_name, email, gender) values (12, 'Caryn', 'Crackel', 'ccrackelb@independent.co.uk', 'Genderqueer');
+insert into author (id, first_name, last_name, email, gender) values (13, 'Peggy', 'Pendleton', 'ppendletonc@ameblo.jp', 'Female');
+insert into author (id, first_name, last_name, email, gender) values (14, 'Thomas', 'Duddy', 'tduddyd@blogs.com', 'Male');
+insert into author (id, first_name, last_name, email, gender) values (15, 'Andris', 'Gilbanks', 'agilbankse@census.gov', 'Male');
+insert into author (id, first_name, last_name, email, gender) values (16, 'Alvis', 'Winslade', 'awinsladef@vk.com', 'Male');
+insert into author (id, first_name, last_name, email, gender) values (17, 'Julita', 'Hayden', 'jhaydeng@cnn.com', 'Female');
+insert into author (id, first_name, last_name, email, gender) values (18, 'Granger', 'Focke', 'gfockeh@telegraph.co.uk', 'Male');
+insert into author (id, first_name, last_name, email, gender) values (19, 'Dorise', 'Venney', 'dvenneyi@java.com', 'Female');
+insert into author (id, first_name, last_name, email, gender) values (20, 'Alys', 'Fullilove', 'afullilovej@1und1.de', 'Genderfluid');
+insert into author (id, first_name, last_name, email, gender) values (21, 'Ginnie', 'Carmen', 'gcarmenk@trellian.com', 'Female');
+insert into author (id, first_name, last_name, email, gender) values (22, 'Will', 'Pithie', 'wpithiel@facebook.com', 'Male');
+insert into author (id, first_name, last_name, email, gender) values (23, 'Rustie', 'Liven', 'rlivenm@tinypic.com', 'Male');
+insert into author (id, first_name, last_name, email, gender) values (24, 'Pegeen', 'Hanssmann', 'phanssmannn@uol.com.br', 'Female');
+insert into author (id, first_name, last_name, email, gender) values (25, 'Farrah', 'Waplington', 'fwaplingtono@wordpress.org', 'Female');
---
+++ MockData/checkouts.sql
@@ -0,0 +1,32 @@
+create table checkout (
+	id INT,
+	book_id INT,
+	patron_id INT,
+	checkout_date DATE,
+	return_date DATE
+);
+insert into checkout (id, book_id, patron_id, checkout_date, return_date) values (1, 16, 41, '5/29/2023', '2/19/2023');
+insert into checkout (id, book_id, patron_id, checkout_date, return_date) values (2, 37, 24, '5/16/2023', '11/8/2022');
+insert into checkout (id, book_id, patron_id, checkout_date, return_date) values (3, 19, 36, '3/23/2023', '2/11/2023');
+insert into checkout (id, book_id, patron_id, checkout_date, return_date) values (4, 46, 47, '2/2/2023', '10/6/2022');
+insert into checkout (id, book_id, patron_id, checkout_date, return_date) values (5, 21, 47, '3/3/2023', '4/29/2023');
+insert into checkout (id, book_id, patron_id, checkout_date, return_date) values (6, 7, 29, '11/10/2022', '4/19/2023');
+insert into checkout (id, book_id, patron_id, checkout_date, return_date) values (7, 20, 24, '5/26/2023', '10/24/2023');
+insert into checkout (id, book_id, patron_id, checkout_date, return_date) values (8, 3, 34, '12/10/2022', '5/23/2023');
+insert into checkout (id, book_id, patron_id, checkout_date, return_date) values (9, 48, 0, '10/5/2022', '9/20/2023');
+insert into checkout (id, book_id, patron_id, checkout_date, return_date) values (10, 24, 17, '6/24/2023', '9/5/2023');
+insert into checkout (id, book_id, patron_id, checkout_date, return_date) values (11, 27, 25, '2/15/2023', '10/22/2022');
+insert into checkout (id, book_id, patron_id, checkout_date, return_date) values (12, 47, 8, '6/6/2023', '9/15/2023');
+insert into checkout (id, book_id, patron_id, checkout_date, return_date) values (13, 30, 3, '11/10/2022', '8/27/2023');
+insert into checkout (id, book_id, patron_id, checkout_date, return_date) values (14, 23, 18, '3/30/2023', '8/14/2023');
+insert into checkout (id, book_id, patron_id, checkout_date, return_date) values (15, 32, 21, '3/28/2023', '12/1/2022');
+insert into checkout (id, book_id, patron_id, checkout_date, return_date) values (16, 42, 36, '7/16/2023', '10/4/2023');
+insert into checkout (id, book_id, patron_id, checkout_date, return_date) values (17, 49, 35, '7/27/2023', '6/10/2023');
+insert into checkout (id, book_id, patron_id, checkout_date, return_date) values (18, 26, 1, '9/10/2022', '12/21/2022');
+insert into checkout (id, book_id, patron_id, checkout_date, return_date) values (19, 22, 10, '8/6/2023', '11/28/2022');
+insert into checkout (id, book_id, patron_id, checkout_date, return_date) values (20, 22, 28, '7/12/2023', '5/24/2023');
+insert into checkout (id, book_id, patron_id, checkout_date, return_date) values (21, 7, 22, '7/12/2023', '12/28/2022');
+insert into checkout (id, book_id, patron_id, checkout_date, return_date) values (22, 46, 41, '7/31/2023', '10/21/2023');
+insert into checkout (id, book_id, patron_id, checkout_date, return_date) values (23, 28, 48, '8/22/2023', '2/21/2023');
+insert into checkout (id, book_id, patron_id, checkout_date, return_date) values (24, 12, 7, '8/31/2023', '3/27/2023');
+insert into checkout (id, book_id, patron_id, checkout_date, return_date) values (25, 4, 12, '7/29/2023', '9/8/2023');
